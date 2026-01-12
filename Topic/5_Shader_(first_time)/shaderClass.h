17693e96a3961e31230a99f82b2b56a399d7c28e
Thawiat00 <Thawiat01@gmail.com>
2026-01-12 22:58:43 +0700

    update add checkcomplieError
    update add checkcomplieError

--- Topic/5_Shader_(first_time)/shaderClass.h
+++ Topic/5_Shader_(first_time)/shaderClass.h
@@ -22,7 +22,9 @@ class Shader
 		void Activate();
 		void Delete();
 		
-
+	private:
+		// Checks if the different Shaders have compiled properly
+		void compileErrors(unsigned int shader, const char* type);
 };
 
 
